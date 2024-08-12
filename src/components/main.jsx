1be8c2925d3c50e2287ddba64f873123251351f9
RishaanJ <rishaanjain@gmail.com>
2024-08-12 16:02:40 -0700

    main page, can see login + logout

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,15 +1,55 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import {auth, db} from './firebase'
+import {doc, getDoc} from 'firebase/firestore'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
 function Main(){
+    const [userDetails, setUserDetails] = useState(null);
+
+    const fetchUserData = async () => {
+        auth.onAuthStateChanged(async(user)=>{
+            console.log(user)
+            const docRef = doc(db, "Users", user.uid)
+            const docSnap = await getDoc(docRef)
+            if(docSnap.exists()){
+                setUserDetails(docSnap.data())
+            } else{
+                console.log("user not logged in")
+            }
+
+        })
+    }
+
+    async function handleLogout(){
+        try{
+            await auth.signOut()
+            window.location.href = "/login"
+            console.log("user logged out successfully");
+        } catch(error){
+            toast.error(error, {position: "top-right"})
+        }
+    }
+
     useEffect(() => {
         document.title = "Main Page - Briefly";
+        fetchUserData();
     }, []);
+
     return (
-        <>
-            <h1>Hey</h1>
-        </>
+        <div>
+            {userDetails ? (
+                <>
+                    <h1>Hey {userDetails.username}</h1>
+                    <button onClick={() => handleLogout()}>Logout</button>
+                </>
+            ) : (
+                <h1>Not Logged In :C</h1>
+            )}
+        <ToastContainer/>
+        </div>
     )
 }
 
